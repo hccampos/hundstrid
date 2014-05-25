bc50f599330cfd960a25412f40220c374f00bd49
Hugo Campos <hcfields@gmail.com>
2014-05-25 12:42:38 +0200

    Refactored the game a bit and added several comments.

--- game/js/game.js
+++ game/js/game.js
@@ -16,17 +16,15 @@ require([
 	"use strict";
 
 	var goo;
+	var loader;
 	var isReady = false;
+
+	// Entities
 	var spaceship = null;
 	var camera = null;
 	var light = null;
+
 	var players = {};
-
-	var $qrCode = $('#qr-code');
-	var $ball = $('#ball');
-	var host = "http://" + window.location.host;
-
-	var qrCode = new QRCode($qrCode[0]);
 
 	initEngine();
 
@@ -52,6 +50,13 @@ require([
 	}
 
 
+	/**
+	 * Called when a player is added to the game.
+	 *
+	 * @param  {object} data
+	 *         The data received from the server which indicates which player
+	 *         was added.
+	 */
 	function onPlayerAdded(data) {
 		if (!data.playerId)
 			return;
@@ -67,6 +72,13 @@ require([
 	}
 
 
+	/**
+	 * Called when a player is removed from the game.
+	 *
+	 * @param  {object} data
+	 *         The data received from the server which indicates which player
+	 *         was removed.
+	 */
 	function onPlayerRemoved(data) {
 		if (!data.playerId || !players.hasOwnProperty(data.playerId))
 			return;
@@ -109,64 +121,20 @@ require([
 		gooCanvas.style.height = '100%';
 		document.body.appendChild(gooCanvas);
 
-		var loader = new DynamicLoader({
+		loader = new DynamicLoader({
 			world: goo.world,
 			rootPath: 'assets'
 		});
 
-		loader.load('root.bundle', {
-			preloadBinaries: true
-		})
-		.then(function (bundle) {
-			var project = null;
-
-			// Try to get the first project in the bundle.
-			for (var key in bundle) {
-				if (/\.project$/.test(key)) {
-					project = bundle[key];
-					break;
-				}
-			}
-
-			if (!project || !project.id) {
-				alert('Error: No project in bundle'); // Should never happen.
-				return null;
-			}
-
-			return loader.load(project.id);
-		})
-		.then(function (configs) {
-			goo.world.processEntityChanges();
-
-			spaceship = goo.world.by.name("spaceship").toArray()[0];
-			camera = goo.world.by.name("camera").toArray()[0];
-			light = goo.world.by.name("light_1").toArray()[0];
-
-			// Remove the spaceship because we only need it to create clones.
-			// We won't actually control it or display it.
-			spaceship.removeFromWorld();
-
-			var renderSystem = goo.world.getSystem('RenderSystem');
-			var promise = new RSVP.Promise();
-
-			goo.renderer.precompileShaders(renderSystem._activeEntities, renderSystem.lights, function () {
-				goo.renderer.preloadMaterials(renderSystem._activeEntities, function () {
-					promise.resolve();
-				});
-			});
-
-			goo.renderer.setClearColor(0, 0, 0, 1.0);
-
-			return promise;
-		})
+		loader.load('root.bundle', { preloadBinaries: true })
+		.then(loadProjectFromBundle)
+		.then(prepareScene)
+		.then(initRenderer)
+		.then(initConnections)
 		.then(function () {
-			goo.world.processEntityChanges();
-			goo.startGameLoop();
-			goo.renderer.domElement.focus();
-
-			initSockets();
+			isReady = true;
 		}, function (error) {
-			console.log(error);
+			console.error(error);
 		});
 	}
 
@@ -174,14 +142,86 @@ require([
 	/**
 	 * Setups all the socket connections and callbacks.
 	 */
-	function initSockets() {
-		var socket = io.connect(host);
+	function initConnections() {
+		var socket = io.connect(getURL());
 		socket.on('command', onCommand);
 		socket.on('playerAdded', onPlayerAdded);
 		socket.on('playerRemoved', onPlayerRemoved);
 		socket.emit('registerGame', null, onRegistered);
+	}
 
-		isReady = true;
+
+	/**
+	 * Loads a project from the specified bundle config.
+	 *
+	 * @param  {object} bundle
+	 *         The bundle configs from where the project is to be extracted.
+	 *
+	 * @return {Promise}
+	 */
+	function loadProjectFromBundle (bundle) {
+		var project = null;
+
+		// Try to get the first project in the bundle.
+		for (var key in bundle) {
+			if (/\.project$/.test(key)) {
+				project = bundle[key];
+				break;
+			}
+		}
+
+		if (!project || !project.id) {
+			throw new Error('Error: no project in bundle.');
+		}
+
+		return loader.load(project.id);
+	}
+
+
+	/**
+	 * Prepares the scene before starting the game loop.
+	 *
+	 * @param  {object} configs
+	 *         The project configs.
+	 *
+	 * @return {object}
+	 *         The config object passed as a parameter.
+	 */
+	function prepareScene(configs) {
+		// Make sure the world has been processed so that we can select the
+		// entities.
+		goo.world.processEntityChanges();
+
+		// Get some entities that we are interested in.
+		spaceship = goo.world.by.name("spaceship").toArray()[0];
+		camera = goo.world.by.name("camera").toArray()[0];
+		light = goo.world.by.name("light_1").toArray()[0];
+
+		// Remove the spaceship because we only need it to create clones.
+		// We won't actually control it or display it.
+		spaceship.removeFromWorld();
+
+		return configs;
+	}
+
+
+	function initRenderer(configs) {
+		var renderSystem = goo.world.getSystem('RenderSystem');
+		var promise = new RSVP.Promise();
+
+		goo.renderer.precompileShaders(renderSystem._activeEntities, renderSystem.lights, function () {
+			goo.renderer.preloadMaterials(renderSystem._activeEntities, function () {
+				goo.world.processEntityChanges();
+				goo.startGameLoop();
+				goo.renderer.domElement.focus();
+
+				promise.resolve();
+			});
+		});
+
+		goo.renderer.setClearColor(0, 0, 0, 1.0);
+
+		return promise;
 	}
 
 
@@ -194,7 +234,10 @@ require([
 	 *         The key used to authenticate players.
 	 */
 	function makeCode(id, key) {
-		var baseUrl = host + '/controller'
+		var $qrCode = $('#qr-code');
+		var qrCode = new QRCode($qrCode[0]);
+
+		var baseUrl = getURL() + '/controller'
 		var encodedId = encodeURIComponent(id);
 		var encodedKey = encodeURIComponent(key);
 
@@ -203,5 +246,15 @@ require([
 		qrCode.makeCode(url);
 
 		$qrCode.attr('href', url);
+	}
+
+
+	/**
+	 * Gets the current URL.
+	 *
+	 * @return {string}
+	 */
+	function getURL() {
+		return 'http://' + window.location.host;
 	}
 });
